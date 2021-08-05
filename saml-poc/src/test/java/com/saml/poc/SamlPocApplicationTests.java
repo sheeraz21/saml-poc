a494a10867092ee579cdd601dc1390cb3cde58fb
Sheeraz <er.syedsheerazshaukat@gmail.com>
2021-08-05 22:44:38 +0530

    saml poc first commit

---
+++ saml-poc/src/test/java/com/saml/poc/SamlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saml.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
